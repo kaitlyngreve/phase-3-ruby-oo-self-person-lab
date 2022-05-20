94d8993ccf74dc820c5628b5d4d2aa548dbb1b15
Kaitlyn Greve <kgreve14@gmail.com>
2022-05-20 16:01:05 -0400

    commit tree 98625b5f2ff31c95375db5f4171522be9019b3a6

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,30 @@
 # your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name, :happiness, :hygiene, :bank_account
+    
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(number)
+        if number > 10
+            10
+        elsif number < 0
+            0
+        else 
+            number
+        end
+end
+
+p = Person.new("Kaitlyn")
+binding.pry
+
+
+
+
+
